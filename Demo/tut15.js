d397889e58f3767cc0fae0b582c13248c3e07bdb
Nikhil <nikhilmaske2001@gmail.com>
2020-11-11 17:40:23 +0530

    CWH tut 15

---
+++ Demo/tut15.js
@@ -0,0 +1,12 @@
+const tut = 15;
+console.log("This is tut "+ tut);
+
+let cot = document.querySelector(".container");
+// console.log(cot.childNodes);
+// console.log(cot.children);
+
+let nodeName = cot.childNodes[0].nodeName;
+// console.log(nodeName);
+
+let container = document.querySelector(".container");
+// console.log(container.children[0].children);
