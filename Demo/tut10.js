a6de5c9bbb67e39ecd1133501c92be5d252cc4a1
Nikhil <nikhilmaske2001@gmail.com>
2020-11-19 09:54:41 +0530

    CHW tut30 skipped

--- Demo/tut10.js
+++ Demo/tut10.js
@@ -1,18 +1,18 @@
-// console.log("This is tut 10");
-// const name = "Nikhil";
-// const name1 = "Vedanti";
+console.log("This is tut 10");
+const name = "Nikhil";
+const name1 = "Vedanti";
 
-// function greet(n){
-//     console.log("Happy birthday " + n);
-// }
-// greet(name);
-// greet(name1);
+function greet(n){
+    console.log("Happy birthday " + n);
+}
+greet(name);
+greet(name1);
 
-// const myobj = {
-//     name : "Nikhil",
-//     surName : "Maske"
-//     marks : [91,65,120000]
-// }
+const myobj = {
+    name : "Nikhil",
+    surName : "Maske"
+    marks : [91,65,120000]
+}
 
-// // console.log(myobj.name);
-// console.log(myobj);
+// console.log(myobj.name);
+console.log(myobj);
---
+++ Demo/tut30.js
@@ -0,0 +1,5 @@
+// Not that important
+const tut = 30;
+console.log("This is tut "+ tut);
+
+//Skipped
