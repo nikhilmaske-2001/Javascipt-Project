1564a27c9e4aa5ad8eee7f9af19f150b8927eb7f
Nikhil <nikhilmaske2001@gmail.com>
2020-11-23 19:21:52 +0530

    CHW 34 completed

---
+++ Demo/tut34.js
@@ -0,0 +1,10 @@
+console.log("This is tut 34"); 
+
+setTimeout(()=>{
+    for (let index = 0; index < 4005; index++) {
+        const element = index;
+        console.log("This is index no "+ index);
+    }
+},100);
+
+console.log("PRinting is done");
